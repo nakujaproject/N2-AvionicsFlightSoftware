54f92e70b2c9129642d4ad3a45f428a511c546e4
ianmuchyri <ianmuchiri8@gmail.com>
2024-01-26 12:48:05 +0300

    add spiff support
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "defs.h"
 #include "kalmanfilter.h"
 #include "functions.h"
+#include "SPIFFS.h"
 
 TimerHandle_t ejectionTimerHandle = NULL;
 
@@ -15,12 +16,15 @@ TaskHandle_t WiFiTelemetryTaskHandle;
 
 TaskHandle_t GetDataTaskHandle;
 
-TaskHandle_t SDWriteTaskHandle;
+TaskHandle_t SPIFFSWriteTaskHandle;
 
 TaskHandle_t GPSTaskHandle;
 
 // if 1 chute has been deployed
 uint8_t isChuteDeployed = 0;
+
+/* Onboard logging */
+File file;
 
 // determines whether we create an access point or connect to one
 int access_point = 0;
@@ -32,11 +36,11 @@ float previousAltitude;
 volatile int state = 0;
 
 static uint16_t wifi_queue_length = 100;
-static uint16_t sd_queue_length = 500;
+static uint16_t spiff_queue_length = 500;
 static uint16_t gps_queue_length = 100;
 
 static QueueHandle_t wifi_telemetry_queue;
-static QueueHandle_t sdwrite_queue;
+static QueueHandle_t spiff_queue;
 static QueueHandle_t gps_queue;
 
 // callback for done ejection
@@ -99,7 +103,7 @@ void GetDataTask(void *parameter)
     {
       droppedWiFiPackets++;
     }
-    if (xQueueSend(sdwrite_queue, (void *)&ld, 0) != pdTRUE)
+    if (xQueueSend(spiff_queue, (void *)&ld, 0) != pdTRUE)
     {
       droppedSDPackets++;
     }
@@ -167,7 +171,7 @@ void readGPSTask(void *parameter)
   }
 }
 
-void SDWriteTask(void *parameter)
+void SPIFFSWriteTask(void *parameter)
 {
 
   struct Data ld = {0};
@@ -175,13 +179,21 @@ void SDWriteTask(void *parameter)
   struct GPSReadings gps = {0};
   float latitude = 0;
   float longitude = 0;
+  char telemetry_data[180];
+  int id = 0;
+
+  file = SPIFFS.open("/log.csv", FILE_APPEND);
+  if (!file)
+    debugln("[-] Failed to open file for appending");
+  else
+    debugln("[+] File opened for appending");
 
   for (;;)
   {
 
     for (int i = 0; i < 5; i++)
     {
-      xQueueReceive(sdwrite_queue, (void *)&ld, 10);
+      xQueueReceive(spiff_queue, (void *)&ld, 10);
 
       ldRecords[i] = ld;
       ldRecords[i].latitude = latitude;
@@ -194,7 +206,32 @@ void SDWriteTask(void *parameter)
       }
     }
 
-    appendToFile(ldRecords);
+    sprintf(telemetry_data,
+            "%i,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%i,%.2f,%.2f,%.2f,%.16f,%.16f\n",
+            id,            // 0
+            ld.ax,         // 1
+            ld.ay,         // 2
+            ld.az,         // 3
+            ld.gx,         // 4
+            ld.gy,         // 5
+            ld.gz,         // 6
+            ld.state,      // 7
+            ld.filtered_s, // 8
+            ld.filtered_v, // 9
+            ld.filtered_a, // 10
+            gps.latitude,  // 11
+            gps.longitude  // 12
+    );
+
+    if (file.print(telemetry_data))
+    {
+      debugln("[+] Message appended");
+    }
+    else
+    {
+      debugln("[-] Append failed");
+    }
+    file.close();
 
     // yield to GPS Task
     vTaskDelay(1000 / portTICK_PERIOD_MS);
@@ -225,20 +262,24 @@ void setup()
 
   init_sensors();
 
-  // initSDCard();
+  // setup flash memory
+  if (!SPIFFS.begin(true))
+    debugln("[-] An error occurred while mounting SPIFFS");
+  else
+    debugln("[+] SPIFFS mounted successfully");
 
   // get the base_altitude
   BASE_ALTITUDE = get_base_altitude();
 
   wifi_telemetry_queue = xQueueCreate(wifi_queue_length, sizeof(Data));
-  sdwrite_queue = xQueueCreate(sd_queue_length, sizeof(Data));
+  spiff_queue = xQueueCreate(spiff_queue_length, sizeof(Data));
   gps_queue = xQueueCreate(gps_queue_length, sizeof(GPSReadings));
 
   // initialize core tasks
   xTaskCreatePinnedToCore(GetDataTask, "GetDataTask", 3000, NULL, 1, &GetDataTaskHandle, 0);
   xTaskCreatePinnedToCore(WiFiTelemetryTask, "WiFiTelemetryTask", 4000, NULL, 1, &WiFiTelemetryTaskHandle, 0);
-  //xTaskCreatePinnedToCore(readGPSTask, "ReadGPSTask", 3000, NULL, 1, &GPSTaskHandle, 1);
-  // xTaskCreatePinnedToCore(SDWriteTask, "SDWriteTask", 4000, NULL, 1, &SDWriteTaskHandle, 1);
+  xTaskCreatePinnedToCore(readGPSTask, "ReadGPSTask", 3000, NULL, 1, &GPSTaskHandle, 1);
+  xTaskCreatePinnedToCore(SPIFFSWriteTask, "SPIFFSWriteTask", 4000, NULL, 1, &SPIFFSWriteTaskHandle, 1);
 
   vTaskDelete(NULL);
 }
